e6453f3da9f95b346af9ee96ef1d1d081697ab5d
Aensolgon <leha.nov98@mail.ru>
2019-06-04 15:34:29 +0300

    create menu

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ jQuery(function($) {
     $('.menu-btn').click(function(){
     if($('nav').hasClass('push-left')){
         $('nav').removeClass('push-left');
+        $('nav').css('background','#ddd');
         $(this).addClass('menu-btn-active');
+        $('.logo').css('opacity','0');
+        $('nav').addClass('open');
     } else {
     $('nav').addClass('push-left');
     $(this).removeClass('menu-btn-active');
+    $('.logo').css('opacity','1');
+    $('nav').removeClass('open');
+    $('nav').css('background','#fff');
     }
     });
 });
